527178e856d78e397ba03caba7dd6ad1be82ed09
Vitalii Kalchuk <v.sweak@gmail.com>
2018-07-15 23:44:34 +0300

    add icon lib

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Modal, View, Image, Text, Button, StyleSheet } from "react-native";
+import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const placeDetail = props => {
   let modalContent = null;
@@ -21,7 +23,12 @@ const placeDetail = props => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
-          <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+          <TouchableOpacity onPress={props.onItemDeleted}>
+            <View style={{alignItems: 'center'}}>
+              <Icon size={30} name='ios-trash' color='red'/>
+            </View>  
+          </TouchableOpacity>
+
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
       </View>
